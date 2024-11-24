479849768525abca56821ea89d7301985d790378
abidevnyc <ab@idev.nyc.mn>
2024-11-24 21:11:55 +0800

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+#python是一种面向对象的解释型计算机程序设计语言
+#python是强类型的动态脚本语言
+print("hello world")
+#bug
+'''
+print(“chinese character”)
+#SyntaxError: invalid character '“' (U+201C)   chinese character
+    print("IndentationError: unexpected indent")
+print("SyntaxError: invalid syntax")print("SyntaxError: invalid syntax")
+print(SyntaxError invalid syntax. Perhaps you forgot a comma)
+'''
+print("print","multilpe","value")
+print("print""multilpe""value")
+print("print","multilpe","value",sep='|')
+print("one",end="")
+print("two")
+print("one",end="\n")
+print("two")
+
